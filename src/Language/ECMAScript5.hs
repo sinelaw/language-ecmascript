7c706a0f053f27037ec5d6882351376646c67c79
Andrey Chudnov <oss@chudnov.com>
2013-10-15 16:07:02 -0400

    Re-export more modules in Language.ECMAScript5

--- src/Language/ECMAScript5.hs
+++ src/Language/ECMAScript5.hs
@@ -2,11 +2,13 @@
 module Language.ECMAScript5
   ( module Language.ECMAScript5.Syntax
   , module Language.ECMAScript5.Parser
---  , renderStatements
---  , renderExpression
+  , module Language.ECMAScript5.PrettyPrint
+  , module Language.ECMAScript5.Syntax.Arbitrary
+  , module Language.ECMAScript5.Syntax.Annotations
   ) where
 
-
 import Language.ECMAScript5.Syntax
+import Language.ECMAScript5.Syntax.Arbitrary
+import Language.ECMAScript5.Syntax.Annotations
 import Language.ECMAScript5.Parser
 import Language.ECMAScript5.PrettyPrint
