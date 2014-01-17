796afb546706aa577c67b46adeb95b44cbe0fdb1
Andrey Chudnov <oss@chudnov.com>
2014-01-16 19:38:33 -0500

    disable QuickCheck tests until the nontermination issue in the AST generator is fixed

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -13,13 +13,13 @@ main = do unittests_ecmascript3 <- tests_unit
           unittests_ecmascript3_diff <- tests_diff
           let propertytest_ecmascript5_pretty = tests_pretty
           
-          defaultMain $ testGroup "all tests" $
-             tests_ecmascript5_parser :
-             tests_ecmascript5_parser_with_autosemi :
-             propertytest_ecmascript5_pretty :
-             unittests_ecmascript3 : 
-             unittests_ecmascript3_diff :
-             []
+          defaultMain $ testGroup "all tests"
+            [tests_ecmascript5_parser
+            ,tests_ecmascript5_parser_with_autosemi
+            --,propertytest_ecmascript5_pretty
+            ,unittests_ecmascript3
+            ,unittests_ecmascript3_diff
+            ]
             
           
 
