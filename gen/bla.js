d4ba9bd9fbbcc4d426f3a2caba7a1429e14de617
Noam Lewis <noam.lewis@elastifile.com>
2015-04-05 17:18:32 +0300

    testing-wip

---
+++ gen/bla.js
@@ -0,0 +1,45 @@
+{
+    "body": [
+        {
+            "expression": {
+                "value": 1,
+                "loc": {
+                    "start": {
+                        "line": 1,
+                        "column": 0
+                    },
+                    "end": {
+                        "line": 1,
+                        "column": 1
+                    },
+                    "source": null
+                },
+                "type": "Literal"
+            },
+            "loc": {
+                "start": {
+                    "line": 1,
+                    "column": 0
+                },
+                "end": {
+                    "line": 2,
+                    "column": 0
+                },
+                "source": null
+            },
+            "type": "ExpressionStatement"
+        }
+    ],
+    "loc": {
+        "start": {
+            "line": 1,
+            "column": 0
+        },
+        "end": {
+            "line": 2,
+            "column": 0
+        },
+        "source": null
+    },
+    "type": "Program"
+}
